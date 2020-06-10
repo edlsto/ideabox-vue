2b318d39cd9d4cb86073246b7a70b2af6d9bb47c
Edward Stoner <edwardlstoner@gmail.com>
2020-06-10 15:45:44 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <header>Ed's ideas</header>
+    <body>
+      <p>Welcome to my ideas site!</p>
+    </body>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
