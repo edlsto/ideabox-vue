90bdea6240b9c57ef3b92a13ad0704536dd1ae25
Edward Stoner <edwardlstoner@gmail.com>
2020-06-10 17:51:34 -0600

    Create form component

---
+++ src/components/Form.vue
@@ -0,0 +1,32 @@
+<template>
+  <form class="idea-form">
+    <div>
+      <label class="form-label" for="title">Idea title</label
+      ><input type="text" />
+    </div>
+    <div>
+      <label class="form-label" for="title">Idea description</label
+      ><textarea type="text" />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.idea-form {
+  text-align: left;
+  width: 20em;
+  margin: 0 auto;
+}
+.form-label {
+  display: block;
+}
+input,
+textarea {
+  width: 100%;
+  outline: none;
+}
+</style>
